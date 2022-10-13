a2c18f7c256b89e62116523e42ccdfce37840b9f
eliaskohut <misty0mist@gmail.com>
2022-10-13 17:14:30 +0200

    created BorrowRepository

---
+++ src/main/java/com/alexandria/alexandria/repositories/BorrowRepository.java
@@ -0,0 +1,9 @@
+package com.alexandria.alexandria.repositories;
+
+import com.alexandria.alexandria.entities.Borrow;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BorrowRepository extends JpaRepository<Borrow, Long> {
+}
