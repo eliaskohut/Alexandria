b6cf722eaac1c0e72b331442c4c22b72e200d1d4
eliaskohut <misty0mist@gmail.com>
2022-10-13 17:15:50 +0200

    created AuthorRepository

---
+++ src/main/java/com/alexandria/alexandria/repositories/AuthorRepository.java
@@ -0,0 +1,9 @@
+package com.alexandria.alexandria.repositories;
+
+import com.alexandria.alexandria.entities.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+}
